7c8897dba1a09edb6e16787052a247037d63a97f
NIKHIL AWARI <nikhilsawari@gmail.com>
2018-04-09 15:55:23 +0530

    Update newTrail.java

--- 1.WindowHandler/src/newTrail.java
+++ 1.WindowHandler/src/newTrail.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
 public class newTrail {
 
 	public static void main(String[] args) {
